0295b9a5f89f47a54746008ef5b63d00496d275c
Philipp Scheede <p.scheede@gmx.de>
2022-04-27 09:25:49 +0200

    Update config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,14 @@
-if set -q SSH_CONNECTION
+set -gx EDITOR (type -p vim)
 
-else
-    wal_fish
-end
+set -gx PYENV_ROOT $HOME/.pyenv
+set -gx fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
-status --is-interactive; and source (pyenv init -|psub)
+status is-login; and pyenv init --path | source
+status is-interactive; and pyenv init - | source
+
+starship init fish | source
+
+bass source ~/.cargo/env
+
+# Created by `pipx` on 2022-01-29 22:53:37
+set PATH $PATH /home/philipp/.local/bin
